cab5b4344137858360ae1a9765c5705c7b9f4de9
Leon <yaroshenko.leonid@gmail.com>
2023-07-02 10:37:11 +0300

    homework OOP game created

---
+++ SupportUnit.java
@@ -0,0 +1,3 @@
+public abstract class SupportUnit extends Unit {
+    
+}
---
+++ Unit.java
@@ -0,0 +1,13 @@
+public abstract class Unit {
+
+    public String name;
+
+    public int health;
+    public int armorClass;
+    public int speed;
+    
+    public int initiative;
+
+    public boolean isAlive;
+    
+}
